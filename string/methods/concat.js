94e738bd080ec7d585d82b57892bfeb0afc4ceaf
pavelusov <ipavelusov@gmail.com>
2017-10-26 19:14:56 +0500

    String.prototype.concat()

---
+++ string/methods/concat.js
@@ -0,0 +1,8 @@
+var str1 = 'first';
+var str2 = 'second';
+var str3 = 'third';
+var space = ' ';
+var allStr = 'all => '.concat(str1, space, str2, space, str3);
+
+console.log(typeof allStr);
+console.log(allStr);
