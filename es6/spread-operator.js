e50b271e94abed2fe652acf80522c10dc7bc93f9
pavelusov <ipavelusov@gmail.com>
2018-06-04 16:57:50 +0500

    spread operator

--- es6/spread-operator.js
+++ es6/spread-operator.js
@@ -1,3 +1,30 @@
 // Оператор распространения ...
+//позволяет объединять содержимое массивов
 
+let rgb = ['red', 'blue', 'green'];
+let bw = ['black', 'white'];
+let colors = [...rgb, ...bw];
 
+let [green] = [...rgb].reverse(); // не меняем исходный массив, ... используем для копирования массива
+
+let directions = (...args) => {
+    let [start, ...remaining] = args;
+
+    let [finish, ...stops] = remaining.reverse();
+    console.log(start);
+    console.log(finish);
+    console.log(remaining);
+    console.log(stops);
+};
+
+directions(1,2,3,4,5);
+
+let morning = {
+    breakfast: "oatmeal",
+    lunch: "peanut butter and jelly"
+};
+let dinner = "mac and cheese";
+let backpackingMeals = {
+    ...morning,
+    dinner
+};
